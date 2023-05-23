651be8b4de6e026eb36e6d2230a6357b495cd9d5
mattishenner <118067835+mattishenner@users.noreply.github.com>
2023-05-23 10:20:17 +0200

    Begyndt på case galleri og responsivitet

---
+++ js/cases-gallery.js
@@ -0,0 +1,48 @@
+const images = document.querySelectorAll('.gallery-img');
+let imgIndex = 0;
+let progress;
+
+const galleryElement = document.getElementById('gallery');
+const progressBarElements = document.querySelectorAll('.progress-bar');
+
+function makeImageActive(index) {
+  // Remove active class from all images
+  images.forEach((image, i) => {
+    image.classList.remove('active');
+    progressBarElements[i].style.transition = 'none';
+    progressBarElements[i].style.width = '0';
+  });
+
+  // Add active class to the new image
+  images[index].classList.add('active');
+
+  // Delay for the CSS change to take effect
+  setTimeout(() => {
+    progressBarElements[index].style.transition = 'width 3s linear';
+    progressBarElements[index].style.width = '90%';
+  }, 20);
+}
+
+function updateImage() {
+  imgIndex++;
+  if (imgIndex === images.length) imgIndex = 0; // Loop back to the first image
+  makeImageActive(imgIndex);
+
+  // Set up the next image change
+  clearTimeout(progress);
+  progress = setTimeout(updateImage, 3000); // Change every 3 seconds
+}
+
+// Initial setup
+makeImageActive(imgIndex);
+progress = setTimeout(updateImage, 3000);
+
+// Event listeners for images
+images.forEach((image, index) => {
+  image.addEventListener('click', () => {
+    clearTimeout(progress); // Clear existing timer
+    imgIndex = index; // Set current image to the clicked one
+    makeImageActive(imgIndex); // Make the clicked image active
+    progress = setTimeout(updateImage, 3000); // Restart the slideshow
+  });
+});
