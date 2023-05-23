08a9e45ff32e647c3796432cba0680c74e957b0d
sarahbirkkjaer <sarahbirkkjaer@gmail.com>
2023-05-23 12:11:29 +0200

    Automatisk genereret nav

--- js/hero-cta.js
+++ js/hero-cta.js
@@ -8,25 +8,29 @@ function setButtonPosition() {
 
 let transitionTimeout;
 
-window.addEventListener('scroll', function() {
-  clearTimeout(transitionTimeout);
+if (ctaPlaceholder) {
+  window.addEventListener('scroll', function() {
+    clearTimeout(transitionTimeout);
 
-  // Hvis vi er scrollet længere ned end 1/3 af vinduet 
-  // Tilføj .scrolled & .transition class
-  if (window.scrollY > window.innerHeight / 3) {
-    ctaBtn.classList.add('scrolled');
-    ctaBtn.style.top = 0+"px";
-    ctaBtn.classList.add('transition');
-  } else {
-    ctaBtn.classList.remove('scrolled');
-    setButtonPosition();
+    // Hvis vi er scrollet længere ned end 1/3 af vinduet 
+    // Tilføj .scrolled & .transition class
+    if (window.scrollY > window.innerHeight / 3) {
+      ctaBtn.classList.add('scrolled');
+      ctaBtn.style.top = 0+"px";
+      ctaBtn.classList.add('transition');
+    } else {
+      ctaBtn.classList.remove('scrolled');
+      setButtonPosition();
 
-    // Fjern transition class efter 750ms
-    transitionTimeout = setTimeout(function() {
-      ctaBtn.classList.remove('transition');
-    }, 750);
-  }
-});
+      // Fjern transition class efter 750ms
+      transitionTimeout = setTimeout(function() {
+        ctaBtn.classList.remove('transition');
+      }, 750);
+    }
+  });
 
-window.addEventListener('resize', setButtonPosition);
-setButtonPosition();
+  window.addEventListener('resize', setButtonPosition);
+  setButtonPosition();
+} else {
+  ctaBtn.classList.add('scrolled');
+}
---
+++ js/nav.js
@@ -0,0 +1,59 @@
+const nav = document.querySelector("nav");
+
+//active link styling
+const currentPage = window.location.pathname;
+console.log(currentPage)
+
+//array til nav items
+let navItems = [
+    {title: "Priser", path: "/price.html"},
+    {title: "Om os", path: "/om-os.html"},
+];
+
+
+//opret logo
+let logo = document.createElement("a");
+
+logo.href = "/index.html";
+logo.classList.add("logo");
+logo.textContent = "KISS & Co.";
+
+nav.appendChild(logo);
+
+
+//opret ul
+let ul = document.createElement("ul");
+//i hver iteration af loopet, laves et list item med et anchor tag
+navItems.forEach((navItem) => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+
+    a.textContent = navItem.title;
+    a.href = navItem.path;
+    
+    li.appendChild(a);
+    ul.appendChild(li);
+
+    if (navItem.path.includes(`${currentPage}`)){
+        a.classList.add("active");
+    }
+})
+
+nav.appendChild(ul);
+
+
+//button
+let button = document.createElement("button");
+
+button.textContent = "Kontakt os";
+button.classList.add("cta-btn");
+
+nav.appendChild(button);
+
+
+//margin
+let marginDiv = document.createElement("div");
+marginDiv.classList.add("nav-marg");
+nav.appendChild(marginDiv);
+
+
