d63909bcc9000e4a40cfe1de149e467b1c9ffcc8
mattishenner <118067835+mattishenner@users.noreply.github.com>
2023-05-24 16:25:35 +0200

    Merget med main og tilføjet event-listener til mobilversion

--- js/contactform.js
+++ js/contactform.js
@@ -1,16 +1,18 @@
-const openForm = document.querySelector('.cta-btn');
+const openForm = document.querySelectorAll('.cta-btn, .cta-placeholder');
 const popupContainer = document.querySelector(".popup-container");
 const close = document.querySelector(".close-btn");
 
-openForm.addEventListener('click', () => {
-    popupContainer.classList.add('show');
+openForm.forEach(btn => {
+    btn.addEventListener('click', () => {
+        popupContainer.classList.add('show');
+    });
 });
 
-close.addEventListener('click', function(){
+close.addEventListener('click', function() {
     popupContainer.classList.remove('show');
 });
 
-window.onclick = function(e){
+window.onclick = function(e) {
     if(e.target == popupContainer) {
         popupContainer.classList.remove('show');
     }
