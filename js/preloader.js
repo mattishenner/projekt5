6b45b9c86849a5dc1e63e3e0927c48eb651eed70
simonostergaard <iversen96c@gmail.com>
2023-05-25 10:55:55 +0200

    PRELOADER

--- js/preloader.js
+++ js/preloader.js
@@ -1,12 +1,23 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('preloader-container'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'js/preloader.json'
-  });
+var container = document.getElementById("lottie-container");
 
-  window.addEventListener('load', function() {
-    var preloader = document.getElementById('preloader-container');
-    preloader.style.display = 'none';
-  });
+var animation = lottie.loadAnimation({
+  container: container,
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+  path: "js/preloader.json"
+});
+
+function showPage() {
+  var preloader = document.getElementById("preloader");
+  var content = document.getElementById("content");
+
+  preloader.style.display = "none";
+
+  content.style.display = "block";
+}
+
+setTimeout(function() {
+  animation.play();
+  setTimeout(showPage, 3200);
+}, 0);
